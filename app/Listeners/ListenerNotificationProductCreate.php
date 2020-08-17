5c3a48efad7c664cb581d186da431ad08f443ffd
tiago <tiagobrunobfr@gmail.com>
2020-08-17 14:22:26 -0300

    add email by event queue

--- app/Listeners/ListenerNotificationProductCreate.php
+++ app/Listeners/ListenerNotificationProductCreate.php
@@ -4,12 +4,15 @@ namespace App\Listeners;
 
 use App\Events\EventNotificationProductCreate;
 use App\Mail\ProductMail;
+use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Mail;
 
-class ListenerNotificationProductCreate
+class ListenerNotificationProductCreate implements ShouldQueue
 {
+    use Queueable;
+
     /**
      * Create the event listener.
      *
@@ -29,6 +32,6 @@ class ListenerNotificationProductCreate
     public function handle(EventNotificationProductCreate $event)
     {
         $product = $event->getProduct();
-        Mail::to($product->store->email)->send(new ProductMail($product));
+        Mail::to($product->store->email)->queue(new ProductMail($product));
     }
 }
