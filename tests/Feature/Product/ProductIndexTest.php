ba7ba11703dd9869e79acda5b72a4856ec3b21cf
tiago <tiagobrunobfr@gmail.com>
2020-08-17 12:23:00 -0300

    add test search product by name and store_id

--- tests/Feature/Product/ProductIndexTest.php
+++ tests/Feature/Product/ProductIndexTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\Product;
 
 use App\Models\Product;
+use App\Models\Store;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -40,5 +41,39 @@ class ProductIndexTest extends TestCase
         $response->assertStatus(200);
         $responseArray = json_decode($response->getContent());
         $this->assertEquals(0, $responseArray->total);
+    }
+
+    /**
+     * @test
+     */
+    public function should_search_product_by_name_and_return_status_code_200_and_store_searched()
+    {
+        factory(Product::class)->create(['name' => 'Tênis']);
+
+        $data = [
+            'name' => 'Tênis',
+        ];
+
+        $response = $this->json('GET', $this::BASE_URL . $this::PRODUCTS, $data);
+        $response->assertStatus(200)
+            ->assertJson(['data' => [['name' => 'Tênis']]]);
+    }
+
+    /**
+     * @test
+     */
+    public function should_search_product_by_store_id_and_return_status_code_200_and_store_searched()
+    {
+        $store_id = factory(Store::class)->create()->id;
+
+        factory(Product::class)->create(['store_id' => $store_id]);
+
+        $data = [
+            'store_id' => $store_id,
+        ];
+
+        $response = $this->json('GET', $this::BASE_URL . $this::PRODUCTS, $data);
+        $response->assertStatus(200)
+            ->assertJson(['data' => [['store_id' => $store_id]]]);
     }
 }
